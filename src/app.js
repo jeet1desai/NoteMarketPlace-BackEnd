c7c0e86ae449d5ed3cf3f6889d240947ad7685bf
jeet1desai <jeetdesai995@gmail.com>
2022-01-03 09:56:15 +0530

    server

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import logger from 'morgan';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+import indexRouter from './routes/index';
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE, FETCH'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/v1', indexRouter);
+
+export default app;
