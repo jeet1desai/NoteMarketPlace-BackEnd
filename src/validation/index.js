12b8bbdada72c5be743cbd46178924e127b1fa86
jeet1desai <jeetdesai995@gmail.com>
2022-01-04 09:05:10 +0530

    added system config validation

--- src/validation/index.js
+++ src/validation/index.js
@@ -10,3 +10,14 @@ export const isPassword = (password) => {
   var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
   return re.test(password);
 };
+
+
+export const checkSystemConfigForm = (body, files) => {
+  if(!files.note_image || !files.profile_image || !body.email || !body.phone){
+    return false;
+  } 
+  if(!isEmail(body.email)){
+    return false;
+  }
+  return true
+}
