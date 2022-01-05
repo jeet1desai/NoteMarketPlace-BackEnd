1c5d1c24b9aa610b771ef330f05afb6ddb791f28
jeet1desai <jeetdesai995@gmail.com>
2022-01-05 09:38:25 +0530

    update s admin controller

--- src/controllers/sAdmin.js
+++ src/controllers/sAdmin.js
@@ -1,6 +1,10 @@
-import Admin from '../models/admin';
-import { testEnvironmentVariable } from '../settings';
-import { checkSystemConfigForm } from '../validation';
+import bcrypt from 'bcrypt';
+
+import Admin from '../models/Admin';
+import { testEnvironmentVariable, aEmail } from '../settings';
+import { checkSystemConfigForm, isEmail } from '../validation';
+import { sAdminTransport } from '../helper';
+import { TAddAdminEmail } from '../templates';
 
 const adminModel = new Admin();
 export const getSystemConfig = async (req, res) => {
@@ -12,18 +16,17 @@ export const getSystemConfig = async (req, res) => {
       return res.status(404).json({ message: 'Not a valid user' });
     }
     const sConfig = await adminModel.getSystemConfig();
-    // console.log(sConfig);
 
     const resp = {
-      email: null,
-      phone: null,
-      facebook: null,
-      twitter: null,
-      linkedin: null,
-      note_image: null,
-      profile_image: null,
+      email: sConfig.rowCount === 0 ? null : sConfig.rows[0].email,
+      phonenumber: sConfig.rowCount === 0 ? null : sConfig.rows[0].phonenumber,
+      facebookurl: sConfig.rowCount === 0 ? null : sConfig.rows[0].facebookurl,
+      twitterurl: sConfig.rowCount === 0 ? null : sConfig.rows[0].twitterurl,
+      linkedinurl: sConfig.rowCount === 0 ? null : sConfig.rows[0].linkedinurl,
+      notepicture: sConfig.rowCount === 0 ? null : sConfig.rows[0].notepicture,
+      profilepicture:
+        sConfig.rowCount === 0 ? null : sConfig.rows[0].profilepicture,
     };
-
     return res.status(200).json(resp);
   } catch (error) {
     return res.status(500).json({
@@ -40,12 +43,9 @@ export const addSystemConfig = async (req, res) => {
     if (user.rows[0].roleid !== 3) {
       return res.status(404).json({ message: 'Not a valid user' });
     }
-    const files = JSON.parse(JSON.stringify(req.files));
-    const body = JSON.parse(JSON.stringify(req.body));
 
-    if (checkSystemConfigForm(body, files)) {
-      console.log(files, body);
-
+    const body = req.body;
+    if (checkSystemConfigForm(body)) {
       const value = {
         user: id,
         email: body.email,
@@ -53,13 +53,28 @@ export const addSystemConfig = async (req, res) => {
         facebook: body.facebook,
         twitter: body.twitter,
         linkedin: body.linkedin,
-        profile,
-        note,
+        profile: body.profile_image,
+        note: body.note_image,
       };
-
-      return res.status(200).json({ message: 'Submit successfully' });
+      const config = await adminModel.addSystemConfig(value);
+      return res.status(200).json(config.rows[0]);
     }
     return res.status(404).json({ message: 'Enter valid data' });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.stack,
+    });
+  }
+};
+
+export const deleteSystemConfig = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteConfig = await adminModel.deleteSystemConfig(id);
+    if (deleteConfig.rowCount === 0) {
+      return res.status(404).json({ message: 'Data unavailable' });
+    }
+    return res.status(200).json({ message: 'Deleted successfully' });
   } catch (error) {
     return res.status(500).json({
       message: error.stack,
@@ -76,13 +91,141 @@ export const getAdmin = async (req, res) => {
 };
 
 export const addAdmin = async (req, res) => {
-  res.status(200).json({ message: testEnvironmentVariable });
+  const { id } = req;
+  const { firstName, lastName, email, countryCode, phone } = req.body;
+  try {
+    const user = await adminModel.getUserInfo('id', id);
+    if (user.rows[0].roleid !== 3) {
+      return res.status(404).json({ message: 'Not a valid user' });
+    }
+
+    if (!isEmail(email) || phone.length !== 10) {
+      return res.status(404).json({ message: 'Enter valid credential' });
+    }
+
+    const checkIfAdminExist = await adminModel.getUserInfo('email', email);
+    if (checkIfAdminExist.rowCount !== 0) {
+      return res.status(404).json({
+        message: 'The email address already exists!',
+      });
+    }
+
+    var randomPasswordString = Math.random().toString(36).slice(-8);
+    const hashedPassword = await bcrypt.hash(randomPasswordString, 10);
+
+    const fullName = `${firstName} ${lastName}`;
+    const contacts = {
+      from: aEmail,
+      to: email,
+    };
+    const mail = Object.assign(
+      {},
+      TAddAdminEmail.confirm(fullName, email, randomPasswordString),
+      contacts
+    );
+    const info = await sAdminTransport.sendMail(mail);
+
+    if (info.messageId) {
+      const value = {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        countryCode,
+        phone,
+        user: id,
+      };
+      const admin = await adminModel.addAdmin(value);
+      const resp = {
+        id: admin.rows[0].id,
+        email: admin.rows[0].email,
+        firstName: admin.rows[0].firstname,
+        lastName: admin.rows[0].lastname,
+        role: admin.rows[0].roleid,
+        countryCode: admin.rows[0].phonecountrycode,
+        phone: admin.rows[0].phonenumber,
+        isEmailVerified: admin.rows[0].isemailverified,
+        isActive: admin.rows[0].isactive,
+        profilePicture: admin.rows[0].profilepicture,
+      };
+      return res.status(200).json(resp);
+    } else {
+      return res.status(500).json({ message: 'Something went wrong' });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: error.stack,
+    });
+  }
 };
 
 export const editAdmin = async (req, res) => {
-  res.status(200).json({ message: testEnvironmentVariable });
+  const { id } = req;
+  const { uid } = req.params;
+  const { firstName, lastName, email, countryCode, phone } = req.body;
+
+  try {
+    const sAdmin = await adminModel.getUserInfo('id', id);
+    if (sAdmin.rows[0].roleid !== 3) {
+      return res.status(404).json({ message: 'Not a valid user' });
+    }
+
+    const user = await adminModel.getUserInfo('id', uid);
+    if (user.rows[0].roleid !== 2) {
+      return res.status(404).json({ message: 'Not a valid user' });
+    }
+
+    if (!isEmail(email) || phone.length !== 10) {
+      return res.status(404).json({ message: 'Enter valid credential' });
+    }
+
+    const checkIfAdminExist = await adminModel.getUserInfo('email', email);
+    if (
+      checkIfAdminExist.rowCount !== 0 &&
+      checkIfAdminExist.rows[0].id !== parseInt(uid)
+    ) {
+      return res.status(404).json({
+        message: 'The email address already exists!',
+      });
+    }
+
+    const value = {
+      firstName,
+      lastName,
+      email,
+      countryCode,
+      phone,
+      sAdmin: id,
+      user: uid,
+    };
+
+    console.log(value);
+    // const admin = await adminModel.addAdmin(value);
+    // const resp = {
+    //   id: admin.rows[0].id,
+    //   email: admin.rows[0].email,
+    //   firstName: admin.rows[0].firstname,
+    //   lastName: admin.rows[0].lastname,
+    //   role: admin.rows[0].roleid,
+    //   countryCode: admin.rows[0].phonecountrycode,
+    //   phone: admin.rows[0].phonenumber,
+    //   isEmailVerified: admin.rows[0].isemailverified,
+    //   isActive: admin.rows[0].isactive,
+    //   profilePicture: admin.rows[0].profilepicture,
+    // };
+    // return res.status(200).json(resp);
+    res.status(200).json({ message: testEnvironmentVariable });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.stack,
+    });
+  }
+};
+
+export const inactiveAdmin = async (req, res) => {
+  res.status(200).json({ message: 'inactive' });
 };
 
 export const deleteAdmin = async (req, res) => {
-  res.status(200).json({ message: testEnvironmentVariable });
+  res.status(200).json({ message: 'delete' });
 };
