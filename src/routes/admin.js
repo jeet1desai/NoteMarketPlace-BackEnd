3b87a28cae86b4d684c30e53d2967ea2d81b8339
jeet1desai <jeetdesai995@gmail.com>
2022-01-08 10:50:51 +0530

    manage category routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -9,6 +9,11 @@ import {
   getAdmin,
   getAdmins,
   inactiveAdmin,
+  addCategory,
+  editCategory,
+  getCategory,
+  inactiveCategory,
+  getCategories,
 } from '../controllers';
 import { checkToken } from '../middleware';
 
@@ -20,5 +25,23 @@ adminRouter.put('/:uid', checkToken, editAdmin);
 adminRouter.get('/all', checkToken, getAdmins);
 adminRouter.get('/:uid', checkToken, getAdmin);
 adminRouter.delete('/:uid', checkToken, inactiveAdmin);
+
+adminRouter.post('/category', checkToken, addCategory);
+adminRouter.put('/category/:cid', checkToken, editCategory);
+adminRouter.get('/category/all', checkToken, getCategories);
+adminRouter.get('/category/:cid', checkToken, getCategory);
+adminRouter.delete('/category/:cid', checkToken, inactiveCategory);
+
+// adminRouter.post('/', checkToken, addType);
+// adminRouter.put('/:uid', checkToken, editType);
+// adminRouter.get('/all', checkToken, getTypes);
+// adminRouter.get('/:uid', checkToken, getType);
+// adminRouter.delete('/:uid', checkToken, inactiveType);
+
+// adminRouter.post('/', checkToken, addCountry);
+// adminRouter.put('/:uid', checkToken, editCountry);
+// adminRouter.get('/all', checkToken, getCountries);
+// adminRouter.get('/:uid', checkToken, getCountry);
+// adminRouter.delete('/:uid', checkToken, inactiveCountry);
 
 export default adminRouter;
