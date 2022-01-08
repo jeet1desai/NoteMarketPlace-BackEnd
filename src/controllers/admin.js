f10661b339a4942e5a246364fdc50721469228d3
jeet1desai <jeetdesai995@gmail.com>
2022-01-08 15:23:56 +0530

    added country controller

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -264,132 +264,132 @@ export const getTypes = async (req, res) => {
 };
 
 // // Manage Country
-// export const addCategory = async (req, res) => {
-//   const { id } = req;
-//   const { name, description } = req.body;
+export const addCountry = async (req, res) => {
+  const { id } = req;
+  const { name, code } = req.body;
 
-//   try {
-//     const admin = await adminModel.adminInfoByID(id);
-//     if (admin.rowCount === 0) {
-//       return res
-//         .status(404)
-//         .json({ message: 'You are not valid user to perform task' });
-//     }
+  try {
+    const admin = await adminModel.adminInfoByID(id);
+    if (admin.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ message: 'You are not valid user to perform task' });
+    }
 
-//     const value = {
-//       name: name,
-//       description: description,
-//       user: id,
-//     };
+    const value = {
+      name: name,
+      code: code,
+      user: id,
+    };
 
-//     const category = await adminModel.addCategory(value);
-//     return res.status(200).json(category.rows[0]);
-//   } catch (error) {
-//     return res.status(500).json({
-//       message: error.stack,
-//     });
-//   }
-// };
+    const country = await adminModel.addCountry(value);
+    return res.status(200).json(country.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.stack,
+    });
+  }
+};
 
-// export const editCategory = async (req, res) => {
-//   const { id } = req;
-//   const { cid } = req.params;
-//   const { name, description } = req.body;
+export const editCountry = async (req, res) => {
+  const { id } = req;
+  const { cid } = req.params;
+  const { name, code } = req.body;
 
-//   try {
-//     const admin = await adminModel.adminInfoByID(id);
-//     if (admin.rowCount === 0) {
-//       return res
-//         .status(404)
-//         .json({ message: 'You are not valid user to perform task' });
-//     }
+  try {
+    const admin = await adminModel.adminInfoByID(id);
+    if (admin.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ message: 'You are not valid user to perform task' });
+    }
 
-//     const value = {
-//       name: name,
-//       description: description,
-//       user: id,
-//       id: cid,
-//     };
+    const value = {
+      name: name,
+      code: code,
+      user: id,
+      id: cid,
+    };
 
-//     const category = await adminModel.updateCategory(value);
-//     if (category.rowCount === 0) {
-//       return res.status(404).json({ message: 'Category unavailable' });
-//     }
-//     return res.status(200).json(category.rows[0]);
-//   } catch (error) {
-//     return res.status(500).json({
-//       message: error.stack,
-//     });
-//   }
-// };
+    const country = await adminModel.updateCountry(value);
+    if (country.rowCount === 0) {
+      return res.status(404).json({ message: 'Country unavailable' });
+    }
+    return res.status(200).json(country.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.stack,
+    });
+  }
+};
 
-// export const getCategory = async (req, res) => {
-//   const { id } = req;
-//   const { cid } = req.params;
+export const getCountry = async (req, res) => {
+  const { id } = req;
+  const { cid } = req.params;
 
-//   try {
-//     const admin = await adminModel.adminInfoByID(id);
-//     if (admin.rowCount === 0) {
-//       return res
-//         .status(404)
-//         .json({ message: 'You are not valid user to perform task' });
-//     }
+  try {
+    const admin = await adminModel.adminInfoByID(id);
+    if (admin.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ message: 'You are not valid user to perform task' });
+    }
 
-//     const category = await adminModel.getCategory(cid);
-//     if (category.rowCount === 0) {
-//       return res.status(404).json({ message: 'Category unavailable' });
-//     }
-//     return res.status(200).json(category.rows[0]);
-//   } catch (error) {
-//     return res.status(500).json({
-//       message: error.stack,
-//     });
-//   }
-// };
+    const country = await adminModel.getCountry(cid);
+    if (country.rowCount === 0) {
+      return res.status(404).json({ message: 'Country unavailable' });
+    }
+    return res.status(200).json(country.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.stack,
+    });
+  }
+};
 
-// export const inactiveCategory = async (req, res) => {
-//   const { id } = req;
-//   const { cid } = req.params;
+export const inactiveCountry = async (req, res) => {
+  const { id } = req;
+  const { cid } = req.params;
 
-//   try {
-//     const admin = await adminModel.adminInfoByID(id);
-//     if (admin.rowCount === 0) {
-//       return res
-//         .status(404)
-//         .json({ message: 'You are not valid user to perform task' });
-//     }
+  try {
+    const admin = await adminModel.adminInfoByID(id);
+    if (admin.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ message: 'You are not valid user to perform task' });
+    }
 
-//     const category = await adminModel.inactiveCategory(id, cid);
-//     if (category.rowCount === 0) {
-//       return res.status(404).json({ message: 'Category unavailable' });
-//     }
-//     return res.status(200).json(category.rows[0]);
-//   } catch (error) {
-//     return res.status(500).json({
-//       message: error.stack,
-//     });
-//   }
-// };
+    const country = await adminModel.inactiveCountry(id, cid);
+    if (country.rowCount === 0) {
+      return res.status(404).json({ message: 'Country unavailable' });
+    }
+    return res.status(200).json(country.rows[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.stack,
+    });
+  }
+};
 
-// export const getCategories = async (req, res) => {
-//   const { id } = req;
+export const getCountries = async (req, res) => {
+  const { id } = req;
 
-//   try {
-//     const admin = await adminModel.adminInfoByID(id);
-//     if (admin.rowCount === 0) {
-//       return res
-//         .status(404)
-//         .json({ message: 'You are not valid user to perform task' });
-//     }
+  try {
+    const admin = await adminModel.adminInfoByID(id);
+    if (admin.rowCount === 0) {
+      return res
+        .status(404)
+        .json({ message: 'You are not valid user to perform task' });
+    }
 
-//     const categories = await adminModel.getAllCategory();
-//     if (categories.rowCount === 0) {
-//       return res.status(404).json({ message: 'Categories unavailable' });
-//     }
-//     return res.status(200).json(categories.rows);
-//   } catch (error) {
-//     return res.status(500).json({
-//       message: error.stack,
-//     });
-//   }
-// };
+    const countries = await adminModel.getAllCountry();
+    if (countries.rowCount === 0) {
+      return res.status(404).json({ message: 'Countries unavailable' });
+    }
+    return res.status(200).json(countries.rows);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.stack,
+    });
+  }
+};
